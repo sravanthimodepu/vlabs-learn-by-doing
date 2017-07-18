b6d1fe5793197473c6175ebfe71beb51b9d7bec6
kraghupathi <kraghu.mails@gmail.com>
2017-07-18 12:10:39 +0530

    updated labspec and moodle installation scripts

--- scripts/moodle-mediawiki.sh
+++ scripts/moodle-mediawiki.sh
@@ -1,15 +1,19 @@
-mysql -u root -p'Virtual-Labs123!@#' -e "create database mediawiki; GRANT ALL PRIVILEGES ON mediawiki.* TO root@localhost IDENTIFIED BY 'Virtual-Labs123!@#'; FLUSH PRIVILEGES"
-
-
 ############# install mediawiki #####################
 wget https://releases.wikimedia.org/mediawiki/1.28/mediawiki-1.28.2.tar.gz
 tar xvzf mediawiki-*.tar.gz
-sudo mv mediawiki-1.28.2 /etc/mediawiki
-sudo ln -s /etc/mediawiki/ /var/www/html
+sudo mv mediawiki-1.28.2 /etc/wiki
+sudo ln -s /etc/wiki/ /var/www/html
 sudo service httpd restart
 
 sudo service mysqld start 
 
+################# mediawiki database ##################
+mysql -u root -p'Virtual-Labs123!@#' -e "create database mediawiki; GRANT ALL PRIVILEGES ON mediawiki.* TO root@localhost IDENTIFIED BY 'Virtual-Labs123!@#'; FLUSH PRIVILEGES"
+
+wget http://files.vlabs.ac.in/lbd-project/wiki-dump.sql.tar.gz
+tar -xvf wiki-dump.sql.tar.gz
+
+mysql -u root -p'Virtual-Labs123!@#' mediawiki < wiki-dump.sql
 
 
 ###############install moodle ##########################
@@ -19,18 +23,23 @@ yum install php56w-mbstring -y
 yum install php56w-xmlrpc -y
 service httpd restart
 
+wget http://files.vlabs.ac.in/lbd-project/moodle.tar.gz
+tar -xvf moodle.tar.gz
+
+cp -fr moodle/ /var/www/html/moodle
+chown -R root /var/www/html/moodle/
+chmod -R 755 /var/www/html/moodle/
+
+############## moodle database ###############################
 mysql -u root -p'Virtual-Labs123!@#' -e "create database moodle; GRANT ALL PRIVILEGES ON moodle.* TO root@localhost IDENTIFIED BY 'Virtual-Labs123!@#'; FLUSH PRIVILEGES"
 
 mysql -u root -p'Virtual-Labs123!@#' moodle < testmoodle.sql
 
-wget http://download.moodle.org/download.php/direct/stable25/moodle-latest-25.zip
-unzip moodle-latest-25.zip
+############copy moodledata files ########################
+wget http://files.vlabs.ac.in/lbd-project/moodledata.tar.gz
+tar -xvf moodledata.tar.gz
 
-#cp -fr moodle/ /var/www/html/moodle
-chown -R root /var/www/html/moodle/
-chmod -R 755 /var/www/html/moodle/
-
-#mkdir /var/www/moodledata/
-#chmod 777 /var/www/moodledata/
+cp -r moodledata /var/www/
+chmod -R 777 /var/www/moodledata/
 
 
