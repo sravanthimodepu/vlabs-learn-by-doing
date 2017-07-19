17008f663789584b277d0c4d2ca2c8b371d983c3
kraghupathi <kraghu.mails@gmail.com>
2017-07-19 16:05:00 +0530

    updated scripts

--- scripts/moodle-mediawiki.sh
+++ scripts/moodle-mediawiki.sh
@@ -8,38 +8,48 @@ sudo service httpd restart
 sudo service mysqld start 
 
 ################# mediawiki database ##################
-mysql -u root -p'Virtual-Labs123!@#' -e "create database mediawiki; GRANT ALL PRIVILEGES ON mediawiki.* TO root@localhost IDENTIFIED BY 'Virtual-Labs123!@#'; FLUSH PRIVILEGES"
+mysql -u root -p'VLead@123' -e "create database wiki; GRANT ALL PRIVILEGES ON wiki.* TO root@localhost IDENTIFIED BY 'VLead@123'; FLUSH PRIVILEGES"
 
-wget http://files.vlabs.ac.in/lbd-project/wiki-dump.sql.tar.gz
-tar -xvf wiki-dump.sql.tar.gz
 
-mysql -u root -p'Virtual-Labs123!@#' mediawiki < wiki.sql
+
+#wget http://files.vlabs.ac.in/lbd-project/wiki-dump.sql.tar.gz
+#tar -xvf wiki-dump.sql.tar.gz
+
+#mysql -u root -p'Virtual-Labs123!@#' mediawiki < wiki.sql
 
 
 ###############install moodle ##########################
-yum install php56w-gd -y
-yum install php56w-xml -y
-yum install php56w-mbstring -y
-yum install php56w-xmlrpc -y
-service httpd restart
+#yum install php56w-gd -y
+#yum install php56w-xml -y
+#yum install php56w-mbstring -y
+#yum install php56w-xmlrpc -y
+#service httpd restart
 
-wget http://files.vlabs.ac.in/lbd-project/moodle.tar.gz
-tar -xvf moodle.tar.gz
+#wget http://files.vlabs.ac.in/lbd-project/moodle.tar.gz
+#tar -xvf moodle.tar.gz
 
+#cp -fr moodle/ /var/www/html/moodle
+#chown -R root /var/www/html/moodle/
+#chmod -R 755 /var/www/html/moodle/
+
+wget http://download.moodle.org/download.php/direct/stable25/moodle-latest-25.zip
+unzip moodle-latest-25.zip
 cp -fr moodle/ /var/www/html/moodle
 chown -R root /var/www/html/moodle/
 chmod -R 755 /var/www/html/moodle/
 
-############## moodle database ###############################
-mysql -u root -p'Virtual-Labs123!@#' -e "create database moodle; GRANT ALL PRIVILEGES ON moodle.* TO root@localhost IDENTIFIED BY 'Virtual-Labs123!@#'; FLUSH PRIVILEGES"
 
-mysql -u root -p'Virtual-Labs123!@#' moodle < testmoodle.sql
+
+############## moodle database ###############################
+mysql -u root -p'VLead@123' -e "create database moodle; GRANT ALL PRIVILEGES ON moodle.* TO root@localhost IDENTIFIED BY 'VLead@123'; FLUSH PRIVILEGES"
+
+#mysql -u root -p'Virtual-Labs123!@#' moodle < testmoodle.sql
 
 ############copy moodledata files ########################
-wget http://files.vlabs.ac.in/lbd-project/moodledata.tar.gz
-tar -xvf moodledata.tar.gz
+#wget http://files.vlabs.ac.in/lbd-project/moodledata.tar.gz
+#tar -xvf moodledata.tar.gz
 
-cp -r moodledata /var/www/
+mkdir /var/www/moodledata
 chmod -R 777 /var/www/moodledata/
 
 
